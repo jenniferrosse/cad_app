49db91cfab75cf59ee051c4cbe037627c51041d6
Jennifer Rosse <jenniferrosse26@gmail.com>
2016-11-07 14:27:58 -0600

    Changed to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20161104213122) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -28,8 +31,8 @@ ActiveRecord::Schema.define(version: 20161104213122) do
     t.datetime "updated_at",                          null: false
   end
 
-  add_index "admins", ["email"], name: "index_admins_on_email", unique: true
-  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
+  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
 
   create_table "events", force: :cascade do |t|
     t.integer  "gallery_id"
@@ -45,7 +48,7 @@ ActiveRecord::Schema.define(version: 20161104213122) do
     t.text     "date_info"
   end
 
-  add_index "events", ["gallery_id"], name: "index_events_on_gallery_id"
+  add_index "events", ["gallery_id"], name: "index_events_on_gallery_id", using: :btree
 
   create_table "galleries", force: :cascade do |t|
     t.string   "gallery_name"
