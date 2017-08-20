2a1e02633adcd3b756f0d58a024ffa4feb105744
Jennifer Rosse <jenniferrosse26@gmail.com>
2017-08-20 14:26:27 -0500

    Styling homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   def home
     @events = Event.where("start_date >= ?", Date.today).order('start_date ASC, title ASC')
     @exhibitions = Exhibition.where("end_date >= ?", Date.today).order('end_date ASC')
+    @next_2nd_fridays = Event.where("title ILIKE ?", "%2nd Fridays Gallery Night - September%").where("start_date >= ?", Date.today)
   end
 
   def about
